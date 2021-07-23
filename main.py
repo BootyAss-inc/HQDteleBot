442d1713c04aeca9815deadae028745cbbc3c751
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2021-07-24 01:39:38 +0300

    Update main.py
    Добавил возможность полностью очищать корзину и пофиксил ошибку с многопоточностью(но это не точно)

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from telebot import types, TeleBot
-from webserver import keep_alive
 import sqlite3
 
-bot = TeleBot('1919841754:AAHoKroSygO1G3QsJe9R-5QQMtfj5hEsy94')
+# Добавить возможность убирать по кнопке продукт из корзины
+# cmd.execute(f'''UPDATE OrderItems SET amount = amount - 1 
+# WHERE order_id = (SELECT ID FROM Orders where customer_id = \'{id}\')
+# ''')
+# stash = ''
+# conn.commit()
 
-conn = sqlite3.connect('VapesDB', check_same_thread=False)
-cmd = conn.cursor()
-
+TOKEN = '1911620604:AAFfqei4O_4vV_CP84wU0WhFxZHTOW-JURo'
+bot = TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(msg):
+    conn = sqlite3.connect('VapesDB', check_same_thread=True)
+    cmd = conn.cursor()
     id = msg.chat.id
     bot.send_sticker(
         id, 'CAACAgIAAxkBAAECloJg8anh-l2wL1maGWYaJpckn-LgagACkg8AArkiUUhR-T_aqYRkrSAE'
@@ -28,8 +33,18 @@ def welcome(msg):
             msg.from_user, bot.get_me()), parse_mode='html', reply_markup=markup
     )
 
+    cmd.execute(f'select ID from customers where ID = {msg.chat.id}')
+    if not cmd.fetchone():
+        print(id)
+        cmd.execute(
+            f'insert into customers (ID, name) select \'{msg.chat.id}\', \'{msg.from_user.first_name}\'')
+        conn.commit()
+    # conn.close()
+
 
 def getTastes():
+    conn = sqlite3.connect('VapesDB', check_same_thread=True)
+    cmd = conn.cursor()
     return cmd.execute('select taste, name from Products').fetchall()
 
 
@@ -40,6 +55,8 @@ def makeInlKbrdBtn(obj):
 
 
 def getStash(id):
+    conn = sqlite3.connect('VapesDB', check_same_thread=True)
+    cmd = conn.cursor()
     cmd.execute(f'''
         select p.taste, oi.amount, p.price
         from customers as c, products as p, orderItems as oi, orders as o
@@ -59,6 +76,8 @@ def getStash(id):
 
 @bot.message_handler(content_types=['text'])
 def blabla(message):
+    conn = sqlite3.connect('VapesDB', check_same_thread=True)
+    cmd = conn.cursor()
     if message.chat.type == 'private':
         id = message.chat.id
 
@@ -77,18 +96,18 @@ def blabla(message):
                     makeInlKbrdBtn(tastes[len(tastes) - 1])
                 )
 
-            # photo = open(r'D:\TG_bot\Photo\photo.jpg', 'rb')
-            # bot.send_photo(message.chat.id, photo)
+            photo = open(r'D:\TG_bot\Photo\photo.jpg', 'rb')
+            bot.send_photo(id, photo)
             bot.send_message(id, 'Вот что есть в наличии', reply_markup=markup)
 
         elif message.text.lower() == 'показать корзину':
             markup = types.InlineKeyboardMarkup(row_width=1)
 
             itemGood = makeInlKbrdBtn(('Оформить заказ', 'good'))
-            itemBad = makeInlKbrdBtn(('Исправить', 'bad'))
+            # itemBad = makeInlKbrdBtn(('Исправить', 'bad'))
 
             markup.add(itemGood)
-            markup.add(itemBad)
+            # markup.add(itemBad)
 
             str_stash = ''
             stash = getStash(id)
@@ -102,38 +121,63 @@ def blabla(message):
                 bot.send_message(id, str_stash, reply_markup=markup)
 
         elif message.text.lower() == 'очистить корзину':
+            cmd.execute(f'DELETE FROM OrderItems WHERE order_id = (SELECT ID FROM Orders where customer_id = \'{id}\')')
+            cmd.execute(f'DELETE FROM Orders WHERE customer_id = \'{id}\'')
+            conn.commit()
             bot.send_message(id, 'Корзина обнулена')
 
         else:
             bot.send_message(id, 'Я не знаю что ответить 😢')
+    # conn.close()
 
 
 @ bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
+    conn = sqlite3.connect('VapesDB', check_same_thread=True)
+    cmd = conn.cursor()
     id = call.message.chat.id
     if call.message:
         if call.data == 'good':
             send = bot.send_message(
-                id, 'Спасибо за заказ! Напишите адрес доставки и контактный номер телефонa')
-            bot.register_next_step_handler(send, save_adress)
-
+                id, 'Спасибо за заказ! Напишите адрес доставки и контактный номер телефонa через запятую')
+            bot.register_next_step_handler(send, add_order)
+            
         else:
             addItem(call)
             conn.commit()
+    # conn.close()
 
-def save_adress(message):
-    address = message.text
-    cmd.execute(
-        f'insert into customers (address) select {address}')
+
+def add_order(message):
+    conn = sqlite3.connect('VapesDB', check_same_thread=True)
+    cmd = conn.cursor()
+    user_address = message.text
+    cust_id = message.chat.id
+    my_id = 201658148
+    cmd.execute(f'update customers set address = \'{user_address}\' where id = {cust_id}')
+    conn.commit()
+    bot.send_message(
+                cust_id, 'Ваш заказ успешно оформлен, с вами свяжутся в течении дня!')
+    
+    str_stash = ''
+    stash = getStash(cust_id)
+
+    for item in stash:
+        str_stash += f'{item[0]}: {item[1]}\n'
+
+    bot.send_message(my_id, '''
+Новый заказ!
+Имя: {0.first_name}
+'''.format(message.from_user, bot.get_me()) +  f'''Адрес: {user_address}
+Заказ: ''' + str_stash)
+
+
 
 def addItem(call):
+    conn = sqlite3.connect('VapesDB', check_same_thread=True)
+    cmd = conn.cursor()
     cust_id = call.message.chat.id
     product_name = call.data
-
-    cmd.execute(f'select ID from Customers where ID = {cust_id}')
-    if not cmd.fetchone():
-        cmd.execute(
-            f'insert into customers (ID) select {cust_id}')
 
     cmd.execute(f'select ID from Products where name = \'{product_name}\'')
     product_id = cmd.fetchone()
@@ -163,6 +207,6 @@ def addItem(call):
         cmd.execute(
             f'update OrderItems set amount=amount+1 where id = {orderItem_id}'
         )
+    conn.commit()
 
-keep_alive()
 bot.polling()
