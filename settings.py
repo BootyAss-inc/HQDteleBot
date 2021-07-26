3f70a8054fab434817b823b9e2e8743b9adcefd8
BootyAss <panda_canniball@mail.ru>
2021-07-26 07:24:54 +0300

    setting file

---
+++ settings.py
@@ -0,0 +1,7 @@
+settings = {
+    'token': '1816994699:AAEfS2FoGZW4mNqCswwgvr0SXSa7_k4i7FI',
+    'name': 'test_hqd_bot',
+    'callname': 'BaronBot',
+    'db': 'test.db',
+    'description': ''
+}
